c3e6e6ac62ca4fe62903c4fd7848e178360d3e3f
mdzafri64 <mdzafri64@gmail.com>
2023-11-27 07:24:32 +0530

    learnt object in arrow function

--- training/object/2.js
+++ training/object/2.js
@@ -1,14 +1,14 @@
 let studentData = {
-    studentName: 'zafri',
-    isMoniter: false,
-    roll: 20,
-    studentSubject: 'science',
-    class: '12th',
-    deciplineType: 'very good',
-}
-studentData.studentName='md zafri'
-studentData.class='11th'
-if(studentData.isMoniter == true){
-   studentData.deciplineType='excelent' 
+  studentName: "zafri",
+  isMoniter: false,
+  roll: 20,
+  studentSubject: "science",
+  class: "12th",
+  deciplineType: "very good",
+};
+studentData.studentName = "md zafri";
+studentData.class = "11th";
+if (studentData.isMoniter == true) {
+  studentData.deciplineType = "excelent";
 }
 console.log(studentData);
